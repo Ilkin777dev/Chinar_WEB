855c170befb4c0787135297a5551d7b7860a1f80
rauf <raufasadov23@gmail.com>
2024-07-31 21:49:18 +0400

    changes

--- src/main.py
+++ src/main.py
@@ -246,26 +246,26 @@ def create_initial():
     if db.session.query(Event).count() == 0:
         event1 = Event(
             en_title="DINING ROOMS", az_title="DINING ROOMS",
-            az_content="İki dəbdəbəli yemək zalımız xüsusi tədbirlər keçirmək üçün mükəmməldir. Hər zal 16 qonağı qəbul edə bilər.",
+            az_content="İki lüks zalımız xüsusi tədbirlər keçirmək üçün mükəmməldir. Hər zal 16 qonağı qəbul edə bilər.",
             en_content="Our two luxurious dining rooms are perfect for hosting private events. Each room can accommodate up to 16 guests.",
             image="vip_lounge.png")
         event2 = Event("DRAGON LOUNGE","DRAGON LOUNGE",
-                    "Öz böyük pilləkəni ilə cəlb edən Dragon Lounge asma qatda yerləşir. Zal 80 nəfərə qədər qonaq qəbul edə bilər.",
+                    "Böyük pilləkənləri ilə cəlb edən Dragon Lounge ikinci mərtəbədə yerləşir. 80 nəfərə qədər qonaq qəbul edə bilər.",
                     "Located on the mezzanine and accessed via its own grand staircase. The lounge can accommodate up to 80 guests for a seated dinner.",
                     "dragon_lounge.png")
         event3 = Event("PAN ASIAN DINING","PAN ASIAN DINING",
-                    "Bu sahə yerli üslubları Asiya təsirləri ilə birləşdirir, tədbirlər üçün mükəmməldir. O, geniş açıq mətbəxə və zərif bambuk dekora malikdir. Cazibədar atmosfer 102-dən çox qonağı qəbul edə bilər",
+                    "PAN ASIAN DINING yerli üslubları Asiya təsirləri ilə birləşdirir və tədbirlər üçün mükəmməl məkandır. Burada geniş açıq mətbəx və zərif bambuk dekorları mövcuddur. Rahat atmosfer 102 nəfərdən çox qonağı qəbul edə bilər.",
                     "This area blends local styles with Asian influences, perfect for events. It features a spacious open kitchen and elegant bamboo décor. The inviting atmosphere can accommodate over 102 guests.",
                     "chinarDining_slider_bg.png")
         
         event4 = Event("BAMBOO BAR", "BAMBOO BAR",
-                    "Bamboo Bar görüşlər və xüsusi tədbirlər üçün ideal yerdir. 48 nəfərə qədər qonaq qəbul edə bilən bu, istənilən hadisə üçün mükəmməl məkandır.",
+                    "Bamboo Bar görüşlər və xüsusi tədbirlər üçün ideal yerdir. 48 nəfərə qədər qonaq qəbul edə bilən Bamboo Bar istənilən hadisə üçün mükəmməl məkandır.",
                     "The Bamboo Bar is an ideal spot for gatherings and special events. With the capacity to host up to 48 guests, it’s a perfect venue for any occasion.",
                     "bambooBar_slider_bg.png")
         
         event5 = Event("THE TERRACE", "THE TERRACE",
-                    "Məkanımız açıq terras və bambuk ağacları ilə parlaq və təravətli atmosfer təqdim edir. Teras 120 nəfərə qədər qonaq qəbul edə bilər və bu, istənilən tədbir üçün mükəmməl şərait yaradır.",
-                        "Our venue offers a bright and fresh atmosphere with an open terrace and a bamboo grove. The terrace can accommodate up to 120 guests, providing a perfect setting for any gathering.",
+                    "Məkanımız açıq terras və çinar ağacları ilə parlaq və təravətli atmosfer təqdim edir. Terras 120 nəfərə qədər qonaq qəbul edə bilər və bu, istənilən tədbir üçün mükəmməl şərait yaradır.",
+                        "Our venue offers a bright and fresh atmosphere with an open terrace and a chinar grove. The terrace can accommodate up to 120 guests, providing a perfect setting for any gathering",
                         "terrace_bg.png")
         db.session.add(event1)
         db.session.add(event2)
